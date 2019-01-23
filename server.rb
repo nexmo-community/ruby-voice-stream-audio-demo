640884b04a30d5acc30781a9cc6958266513d61b
benhayehudi <bengreenberg@gmail.com>
2019-01-23 08:40:02 +0200

    migrating code sample from blog posts repo to its own nexmo-community repo

---
+++ server.rb
@@ -0,0 +1,50 @@
+require 'sinatra'
+require 'nexmo'
+require 'dotenv'
+require 'json'
+
+# Load environment variables
+Dotenv.load
+
+# Set Up Nexmo Credentials
+client = Nexmo::Client.new(
+  api_key: ENV['api_key'],
+  api_secret: ENV['api_secret'],
+  application_id: ENV['application_id'],
+  private_key: File.read(ENV['private_key_path'])
+)
+
+# Audio File
+AUDIO_URL = 'http://EXTERNALLY ACCESSIBLE URL HERE/voice_api_audio_streaming.mp3'
+
+# Server Routes
+get '/new' do
+  # Start call
+  response = client.calls.create(
+    to: [{ type: 'phone', number: ENV['sender_phone'] }],
+    from: { type: 'phone', number: ENV['recipient_phone'] },
+    answer_url: ['http://EXTERNALLY ACCESSIBLE URL HERE/answer'],
+    event_url: ['http://EXTERNALLY ACCESSIBLE URL HERE/event']
+  )
+  puts response.inspect
+end
+
+get '/answer' do
+    # Provide the Nexmo Call Control Object (NCCO) as JSON to the Nexmo Platform
+    content_type :json
+    [{ :action => 'stream', :streamUrl => ['http://EXTERNALLY ACCESSIBLE URL HERE/stream/silent'], :loop => 0 }].to_json
+end
+
+get '/stream/silent' do
+    # Stream a silent file in the background to keep the call open
+    send_file File.join(settings.public_folder, 'silence.mp3')
+end
+
+post '/event' do
+  # Get call status
+  data = JSON.parse(request.body.read)
+  # Stream audio if call is answered
+  response = client.calls.stream.start(data['uuid'], stream_url: [AUDIO_URL]) if data['status'] == 'answered'
+  # Print out the response from the Nexmo platform
+  puts response.inspect
+end
